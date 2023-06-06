6a4bc34cf7924e214a0c841278a0aff9f08c783f
Deekshant Yadav <contactdeekshant+git@gmail.com>
2023-06-06 20:17:11 +0530

    Added basic data read logic

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Homepage</title>
+</head>
+<body>
+	<div class="tweets-list">
+		<?php 
+			require_once "config.php";
+
+			$read_sql = "SELECT * FROM tweets";
+			if ($result = mysqli_query($connection, $read_sql)) {
+				if (mysqli_num_rows($result) > 0) {
+					while ($row = mysqli_fetch_array($result)) {
+						echo $row["body"]."<br />";
+					}
+				} else {
+					echo "No tweets found.";
+				}
+				mysqli_free_result($result);
+			}
+			mysqli_close($connection);
+		?>
+	</div>
+</body>
+</html>
