9bdb6aac8a66d9bf1a4e4d81abe0f7d1a852a1fd
Deekshant Yadav <contactdeekshant+git@gmail.com>
2023-06-09 17:13:44 +0530

    Updated imports

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Homepage</title>
-	<link rel="stylesheet" href="./styles/common.css">
-	<link rel="stylesheet" href="./styles/index.css">
+	<link rel="stylesheet" href="./css/common.css">
+	<link rel="stylesheet" href="./css/index.css">
 	<script src="./js/index.js" defer></script>
 </head>
 <body>
--- tweet.php
+++ tweet.php
@@ -49,8 +49,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Homepage</title>
-	<link rel="stylesheet" href="./styles/common.css">
-	<link rel="stylesheet" href="./styles/index.css">
+	<link rel="stylesheet" href="./css/common.css">
+	<link rel="stylesheet" href="./css/index.css">
 	<script src="./js/index.js" defer></script>
 </head>
 <body>
